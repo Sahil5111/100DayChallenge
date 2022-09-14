8a9771eaeded623b429895f41c3a867affeb72e7
Sahil <sahilsingh7977@gmail.com>
2022-09-14 22:00:59 +0530

    added some code

---
+++ day4/script.js
@@ -0,0 +1,20 @@
+const text = document.querySelector("input")
+const button = document.querySelector(".submit")
+
+button.addEventListener("click", () => {
+    if (text.value === "") {
+        console.log("invalid input")
+    } else {
+        div = document.createElement("div")
+        div.textContent = text.value
+        btn = document.createElement("button")
+        btn.setAttribute("class", "delete btn btn-danger my-2");
+        btn.textContent="delete"
+        div.appendChild(btn)
+        btn.addEventListener("click",()=>{
+            console.log("working")
+        })
+        document.body.appendChild(div)
+        text.value = ""
+    }
+})
