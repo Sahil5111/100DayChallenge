69854b4e7d861ec18e168196bde65d95b1598e0f
Sahil <sahilsingh7977@gmail.com>
2022-09-16 17:24:38 +0530

    completed js for day 4 challenge

--- day4/script.js
+++ day4/script.js
@@ -1,7 +1,6 @@
 const text = document.querySelector("input")
 const button = document.querySelector(".submit")
 
-
 button.addEventListener("click", () => {
     if (text.value === "") {
         console.log("invalid input")
@@ -12,10 +11,9 @@ button.addEventListener("click", () => {
         btn.setAttribute("class", "delete btn btn-danger my-2");
         btn.textContent="delete"
         div.appendChild(btn)
-        btn.addEventListener("click",()=>{
-            par=btn.parentElement
-            console.log(par)
-            par.style.display="none"
+        btn.addEventListener("click",e =>{
+            par=e.target.parentElement
+            par.remove()
         })
         document.body.appendChild(div)
         text.value = ""
