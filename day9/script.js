fe62fbcc4d6a328909c88e3418588e6be0109e25
Sahil <sahilsingh7977@gmail.com>
2022-09-19 20:11:40 +0530

    completed js

--- day9/script.js
+++ day9/script.js
@@ -1,1 +1,14 @@
-console.log("hello world")
+const template = document.querySelector("template")
+const div = document.querySelector(".row")
+const button = document.querySelector(".btn")
+const input = document.querySelectorAll("input")
+
+
+button.addEventListener("click", () => {
+    temp = document.importNode(template.content, true)
+    temp.querySelector(".card-title").textContent = input[0].value
+    temp.querySelector("p").textContent = input[1].value
+    div.appendChild(temp)
+    input[0].value=input[1].value=""
+}
+)
