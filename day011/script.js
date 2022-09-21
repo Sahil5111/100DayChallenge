3eb3c89b2b3b5ebf9418d253077476ca55c7fd5d
Sahil <sahilsingh7977@gmail.com>
2022-09-21 20:24:41 +0530

    completed day 11 challenge

--- day011/script.js
+++ day011/script.js
@@ -1,1 +1,13 @@
 console.log('hello world')
+
+const modal = document.querySelector("dialog")
+const start= document.querySelector("#btn")
+const close =document.querySelector(".close")
+
+start.onclick=()=>{
+    modal.show()
+}
+
+close.onclick=()=>{
+    modal.close()
+}
