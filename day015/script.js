79ecf1eef1524c22bcf63ce41c058b744cec742c
Sahil <sahilsingh7977@gmail.com>
2022-09-25 09:17:31 +0530

    completed day 15 challenge

--- day015/script.js
+++ day015/script.js
@@ -1,1 +1,20 @@
-console.log("hello world")
+const search = document.querySelector("input")
+const container = document.querySelector('.container')
+
+fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json()).then(data => {
+    data.forEach(element => {
+        let div = document.createElement("div")
+        div.className = 'col-3'
+        div.textContent = element.username
+        container.appendChild(div)
+    });
+})
+
+
+search.oninput = (e) => {
+    const list = document.querySelectorAll('.col-3')
+    list.forEach(element => {
+        let a = !element.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+        element.classList.toggle("hide", a)
+    });
+}
