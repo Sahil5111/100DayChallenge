e0ff8ba87917c977d43d78d05762bbb6bfd1b340
Sahil <sahilsingh7977@gmail.com>
2022-10-30 23:52:15 +0530

    completed challenge

--- reactchallenge/src/App.js
+++ reactchallenge/src/App.js
@@ -1,7 +1,29 @@
-function App() {
-  return (
-    <>hello world</>
-  )
-}
+import React from "react"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+import data from "./data"
 
-export default App;
+
+export default function App() {
+           const card= data.map((element)=>{
+               return (
+                   <Card
+                    key={element.id}
+                    img={"../images/"+element.coverImg}
+                    rating ={element.stats.rating} 
+                    reviewCount={element.stats.reviewCount}
+                    location={element.location}
+                    title={element.title}
+                    price={element.price}
+                   />
+               )
+           })
+    return (
+        <div>
+            <Navbar />
+            <Hero/>
+            {card}
+        </div>
+    )
+}
