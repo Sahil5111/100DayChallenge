4c51caffba085dff811f3a5a35475f6997abd840
Sahil <sahilsingh7977@gmail.com>
2022-11-08 00:40:18 +0530

    used correct variable names

--- reactrepeat/src/Components/Day002.jsx
+++ reactrepeat/src/Components/Day002.jsx
@@ -4,14 +4,14 @@ export default function Day001() {
     const [changeColor,setChangeColor]=useState("#000000")
 
     function handleClick(event){
-        setChangeColor(e=>event.target.value.toString())
+        setChangeColor(()=>event.target.value.toString())
+        console.log(changeColor)
     }
-
     return (
         <div>
             <div className="d-flex justify-content-centre align-item-centre ">
                 <input type="color" className="color" onChange={handleClick}></input>
-                <div className="container" style={{backgrounColor:changeColor}}></div>
+                <div className="container diplay--color border" style={{backgroundColor:changeColor}}></div>
                 </div>
         </div>
     )
