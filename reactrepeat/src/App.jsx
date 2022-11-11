ab6c5a5eeb3f6551a061256c0d7f11b40bc240ef
Sahil <sahilsingh7977@gmail.com>
2022-11-11 18:04:53 +0530

    repeated day 1 program in rect using usestate hook

--- reactrepeat/src/App.jsx
+++ reactrepeat/src/App.jsx
@@ -1,11 +1,13 @@
 import ColorChanger from "./Components/Day002"
 import Clock from "./Components/Day010"
+import Day001 from "./Components/Day001"
 
 function App() {
   return (
     <div className="App">
       {/* <ColorChanger /> */}
-      <Clock/>
+      {/* <Clock/> */}
+      <Day001/>
     </div>
   )
 }
