e7515588dc138aa5f27910dd93ac8a496762d26c
Sahil <sahilsingh7977@gmail.com>
2022-11-27 17:02:03 +0530

    modified some code

--- djangoapi/djangoapi/settings.py
+++ djangoapi/djangoapi/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^n%ekbh25iay%%tl34$)8ey--%omz4-6^liis3e!9rrk4qjn9h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["firstapi-a7kc.onrender.com",]
 
 
 # Application definition
