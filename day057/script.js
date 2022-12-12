3da7a6d2bb23d3c8bbb54b97b9bf498e33b3c53b
Sahil <sahilsingh7977@gmail.com>
2022-12-12 15:22:07 +0530

    day 68

--- day057/script.js
+++ day057/script.js
@@ -1,11 +1,11 @@
-const col = document.querySelectorAll(".col")
+var col = document.querySelectorAll(".col")
+const container =document.querySelector(".container")
 
 fetch("http://localhost:5000/10")
 .then(response=> response.json())
 .then(response=>{
     let i=0
     response.forEach(ele=>{
-        console.log(ele.url);
         col[i].style.backgroundImage='url('+ele.url.toString()+')'
         i++
     })
@@ -21,3 +21,21 @@ const intersection = new IntersectionObserver(e => {
 col.forEach(card => {
     intersection.observe(card)
 })
+
+const last= new IntersectionObserver(e=>{
+    let lastcard=e[0]
+    if(!lastcard.isIntersecting) return
+    let div=document.createElement('div')
+    div.className='col border'
+    fetch("http://localhost:5000")
+        .then(response => response.json())
+        .then(response => div.style.backgroundImage='url('+response.url.toString()+')')
+    container.appendChild(div)
+    console.log(div)
+    last.unobserve(lastcard.target)
+    intersection.observe(div)
+    col = document.querySelectorAll(".col")
+    last.observe(col[col.length-2])
+})
+
+last.observe(col[col.length-2])
