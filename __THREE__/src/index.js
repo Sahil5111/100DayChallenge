d4d1041e7a011da9893707cd154187dd74b5747d
Sahil <sahilsingh7977@gmail.com>
2022-12-22 18:08:53 +0530

    initalized three js project

---
+++ __THREE__/src/index.js
@@ -0,0 +1,25 @@
+import * as THREE from 'three'
+
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth,window.innerHeight);
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth/window.innerHeight,
+    0.1,
+    1000
+)
+
+renderer.render(scene,camera)
+
+
+
+
+
+
+
+
+
+
+
+document.body.appendChild(renderer.domElement)
---
+++ __THREE__/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path')
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: path.resolve(__dirname, "src/index.js")
+    },
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: `[name][contenthash].js`,
+        clean: true
+    },
+    devtool: 'source-map',
+    plugins: [new HtmlWebpackPlugin(
+        {
+            template: "src/index.html"
+        }
+    )],
+    devServer: {
+        static: { directory: path.resolve(__dirname, "build") },
+        port: 5500,
+        open: true,
+        hot: true,
+        compress: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['css-loader', 'style-loader']
+            },
+            {
+                test: /\.(png|jpg|jpeg|svg|gif)$/i,
+                type: "asset.resource",
+            }
+        ]
+    }
+}
